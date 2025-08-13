a87c91b8ab6e631ccefca9c34235de5e0807cd7b
Abbas-coding <abbasfakhruddin205@gmail.com>
2025-08-13 12:37:13 +0500

    cors handled

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"], // change for prod
+    origin: [process.env.FRONTEND_URL], // change for prod
     methods: ["GET", "POST"]
   }
 });
